c74f45e16c7a3d096ec6f9146915edf3d1e30834
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-22 20:15:03 +0000

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,12 +9,5 @@ export default defineConfig({
       bundler: 'vite',
       viteConfig,
     },
-  },
-
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  }
 });
