1075c57e97feeec1b25f9fe048a4c7bb6d943dab
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-26 22:58:13 +0000

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,15 +3,9 @@ import viteConfig from './vite.config';
 
 export default defineConfig({
   component: {
-    port: 5173,
     devServer: {
       framework: 'react',
       bundler: 'vite',
-      viteConfig,
     },
-  },
-  
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-  },
+  }x
 });
